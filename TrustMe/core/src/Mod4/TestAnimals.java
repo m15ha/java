a4124ad00ea960b42815d0282585d1d6da79e57f
Misha <mihail.lazareff@gmail.com>
2019-09-25 09:22:21 -0400

    2019-09-25

---
+++ TrustMe/core/src/Mod4/TestAnimals.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mod4;
+
+import animals.Animal;
+import animals.Cat;
+import animals.Fish;
+import animals.Spider;
+
+/**
+ *
+ * @author Taurus
+ */
+public class TestAnimals {
+
+    static Cat myCat = new Cat("Tiger");
+    static Animal mySpider = new Spider();
+    static Animal myFish = new Fish();
+
+    public static void main(String[] args) {
+        myCat.setName("Toyger");
+        System.out.println(myCat.getName());
+        myCat.walk();
+        myCat.play();
+        myCat.eat();
+
+        System.out.println("");
+
+        mySpider.walk();
+        mySpider.eat();
+
+        System.out.println("");
+
+        ((Fish)myFish).setName("My Fish");
+        System.out.println(((Fish)myFish).getName());
+        myFish.walk();
+        myFish.eat();
+        ((Fish)myFish).play();
+    }
+
+}
---
+++ TrustMe/core/src/animals/Pet.java
@@ -0,0 +1,10 @@
+package animals;
+
+public interface Pet {
+
+    public String getName();
+
+    public void setName(String n);
+
+    public void play();
+}
