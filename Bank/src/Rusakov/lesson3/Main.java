b37e7c0b91ee5a6766cab3c79389e76caf0b102f
Misha <mihail.lazareff@gmail.com>
2019-10-22 18:54:32 -0400

    2019-10-22 home

--- Bank/src/Rusakov/lesson3/Main.java
+++ Bank/src/Rusakov/lesson3/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         User user1 = new User(1, "Bob", "12345");
         User user2 = new User(2, "Mike", "qwerty");
+        User user3 = new User();
         User[] first = new User[3];
+        user1.setLogin("Boby");
         first[0] = new User (3, "James", "efrftg");
         first[1] = new User (4, "Nade", "eweftg");
         first[2] = new User (5, "Leon", "666663g");
@@ -15,5 +17,9 @@ public class Main {
              ) {
             System.out.println("id = " + j.id + "; Login: " + j.login + "; Password: " + j.password);
         }
+        user1.getInfo();
+        System.out.println(user1.getLogin());
+        user1.whoIsOlder (user2);
+        user3.getInfo();
     }
 }
--- Bank/src/Rusakov/lesson3/User.java
+++ Bank/src/Rusakov/lesson3/User.java
@@ -10,4 +10,45 @@ public class User {
         this.login = login;
         this.password = password;
     }
+
+    public User(){
+        this(18, "Default", "Default");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+
+    }
+
+    public void getInfo() {
+        System.out.println("id = " + id + "; Login: " + login + "; Password: " + password);
+    }
+
+    public void whoIsOlder(User u) {
+        if (this.id > u.id) {
+            System.out.println(this.id + " bigger " + u.id);
+        } else if (this.id < u.id) {
+            System.out.println(this.id + " smaller " + u.id);
+        } else System.out.println(this.id + " the same " + u.id);
+    }
 }
