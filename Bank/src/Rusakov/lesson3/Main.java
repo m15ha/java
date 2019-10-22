cff7956d4625ce6ec27bc16a1c7d32164b7188de
Misha <mihail.lazareff@gmail.com>
2019-10-22 15:47:40 -0400

    2019-10-22 work

--- Bank/src/Rusakov/lesson3/Main.java
+++ Bank/src/Rusakov/lesson3/Main.java
@@ -1,4 +1,19 @@
 package Rusakov.lesson3;
 
 public class Main {
+    public static void main(String[] args) {
+        User user1 = new User(1, "Bob", "12345");
+        User user2 = new User(2, "Mike", "qwerty");
+        User[] first = new User[3];
+        first[0] = new User (3, "James", "efrftg");
+        first[1] = new User (4, "Nade", "eweftg");
+        first[2] = new User (5, "Leon", "666663g");
+     /*   for (int i = 0; i < first.length; i ++){
+            System.out.println(first[i].id);
+        }*/
+        for (User j : first
+             ) {
+            System.out.println("id = " + j.id + "; Login: " + j.login + "; Password: " + j.password);
+        }
+    }
 }
--- Bank/src/Rusakov/lesson3/User.java
+++ Bank/src/Rusakov/lesson3/User.java
@@ -1,4 +1,13 @@
 package Rusakov.lesson3;
 
 public class User {
+    int id;
+    String login;
+    String password;
+
+    public User(int id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
 }
