537df32f8a286a46c48863d56c580b24b0c3b4c2
Misha <mihail.lazareff@gmail.com>
2019-11-03 18:25:16 -0500

    2019-11-03 home

---
+++ Bank/src/Rusakov/Lesson5/Main.java
@@ -0,0 +1,24 @@
+package Rusakov.Lesson5;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String s1 = JOptionPane.showInputDialog(null, "Input fist number");
+        String s2 = JOptionPane.showInputDialog(null, "Input second number");
+        try {
+            int p1 = Integer.parseInt(s1);
+            int p2 = Integer.parseInt(s2);
+            if (p1 <= 0 || p2 >=0) throw new Exception();
+            JOptionPane.showMessageDialog(null, "Total: " + (s1 + s2));
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Wrong simbol");
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Positive only");
+        } finally {
+        }
+    }
+}
