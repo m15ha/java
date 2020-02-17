8d79b2803dbc555d09e205cbb4c90889c07beea5
Misha <mihail.lazareff@gmail.com>
2020-02-17 15:55:37 -0500

    2019-10-25 work

--- Bank/src/Sumin/lesson1/Main.java
+++ Bank/src/Sumin/lesson1/Main.java
@@ -2,11 +2,8 @@ package Sumin.lesson1;
 
 public class Main {
     public static void main(String[] args) {
-        int sec = 500000;
-        int days = sec / 86400;
-        int hours = sec % 86400 - 60;
-        int min = sec - days * 86400 - hours * 60;
-        int secLeft = 5000;
-        System.out.println(hours);
+      for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
     }
 }
